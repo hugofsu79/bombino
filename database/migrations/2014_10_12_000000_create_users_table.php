13587baf170df731400f460ccb638b9082eb7104
hugofsu79 <131622641+hugofsu79@users.noreply.github.com>
2023-08-22 10:17:52 +0200

    projet de départ

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('first_name', 50);
+            $table->string('mail', 50)->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password', 255);
+            $table->char('phone_number', 10);
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->foreignId('role_id')->default(1)->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
